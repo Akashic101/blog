f05b9bf36558e6254ee896a2256d255ce3d78afc
David Moll <david.leander.moll@gmail.com>
2024-01-01 20:34:08 +0100

    Added broken-link fixer

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 const { DateTime } = require("luxon");
 const CleanCSS = require("clean-css");
+const brokenLinksPlugin = require("eleventy-plugin-broken-links");
 const eleventyPluginFilesMinifier = require("@sherby/eleventy-plugin-files-minifier");
 
 const pluginRss = require("@11ty/eleventy-plugin-rss");
@@ -8,15 +9,24 @@ module.exports = function (eleventyConfig) {
 
 	eleventyConfig.addPlugin(pluginRss);
 	eleventyConfig.addPlugin(eleventyPluginFilesMinifier);
+	eleventyConfig.addPlugin(brokenLinksPlugin, {
+		redirect: "warn",
+		broken: "error",
+		cacheDuration: "1d",
+		loggingLevel: 2,
+		excludeUrls: [],
+		excludeInputs: [],
+		callback: null,
+	});
 
 	eleventyConfig.addPassthroughCopy("src/bundle.css");
 	eleventyConfig.addPassthroughCopy("src/assets/fonts/");
 	eleventyConfig.addPassthroughCopy("src/assets/images/");
 	eleventyConfig.addPassthroughCopy("src/assets/icons/");
 
-	eleventyConfig.addFilter("cssmin", function(code) {
+	eleventyConfig.addFilter("cssmin", function (code) {
 		return new CleanCSS({}).minify(code).styles;
-	  });
+	});
 
 	eleventyConfig.addFilter("dateDisplay", (dateObj) => {
 		return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat("LLLL d, y");
@@ -25,7 +35,7 @@ module.exports = function (eleventyConfig) {
 	eleventyConfig.addFilter("currentYear", () => {
 		return new Date().getFullYear();
 	});
-	
+
 	eleventyConfig.addAsyncFilter("joinedTags", async function (tagsObj) { return tagsObj.join(", "); });
 
 	eleventyConfig.addCollection("lastThreeArticles", function (collectionApi) {
