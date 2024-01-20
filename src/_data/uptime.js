eeef92885aac3b35842b132ce2a16f8d3671de2d
David Moll <david.leander.moll@gmail.com>
2024-01-21 00:33:03 +0100

    Added import back

--- src/_data/uptime.js
+++ src/_data/uptime.js
@@ -1,9 +1,11 @@
+require('dotenv').config();
+
 module.exports = async () => {
     const url = 'http://192.168.0.113:8000/uptime';
     const headers = {
         'accept': 'application/json',
         'Authorization': `Bearer ${process.env.BEARER_TOKEN}`
-    }
+    };
 
     try {
         // Use dynamic import for 'node-fetch'
@@ -12,7 +14,7 @@ module.exports = async () => {
         const response = await fetch(url, { method: 'GET', headers: headers });
         const data = await response.json();
 
-        console.log(data)
+        console.log(data);
 
         const dayUptime = calculateTotalUptime(data, 24);  // 24 hours for a day
         const monthUptime = calculateTotalUptime(data, 720);  // 720 hours for a month
@@ -28,17 +30,11 @@ module.exports = async () => {
                     formatted: (monthUptime * 100).toFixed(2)
                 }
             }
-        }
+        };
     } catch (error) {
         console.error('Error:', error.message);
     }
 };
-
-function calculateTotalUptime(data, durationThreshold) {
-    const filteredData = data.filter(record => record.duration === durationThreshold);
-    const totalUptime = filteredData.reduce((sum, record) => sum + record.uptime, 0);
-    return totalUptime;
-}
 
 // Call the function to execute it
 module.exports();
