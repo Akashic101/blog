c03725dbbbc67a4e5971827a7a3b499e4b29c903
David Moll <david.leander.moll@gmail.com>
2024-02-03 12:37:17 +0100

    Added automatic authentication

--- src/_data/uptime.js
+++ src/_data/uptime.js
@@ -1,36 +1,65 @@
 require('dotenv').config();
 
 module.exports = async () => {
-    const url = 'http://192.168.0.113:8000/uptime';
+    const authUrl = 'http://192.168.0.113:8000/login/access-token';
+    const apiUrl = 'http://192.168.0.113:8000/uptime';
     const headers = {
         'accept': 'application/json',
-        'Authorization': `Bearer ${process.env.BEARER_TOKEN}`
+        'Content-Type': 'application/x-www-form-urlencoded',
+    };
+
+    const credentials = {
+        grant_type: '',
+        username: process.env.UPTIME_KUMA_WEB_API_USERNAME,
+        password: process.env.UPTIME_KUMA_WEB_API_PASSWORD,
+        scope: '',
+        client_id: '',
+        client_secret: '',
     };
 
     try {
-        // Use dynamic import for 'node-fetch'
         const fetch = (await import('node-fetch')).default;
 
-        const response = await fetch(url, { method: 'GET', headers: headers });
+        // Get access token
+        const authResponse = await fetch(authUrl, {
+            method: 'POST',
+            headers: headers,
+            body: new URLSearchParams(credentials),
+        });
 
-        if (response.status === 200) {
-            const data = await response.json();
+        if (authResponse.status === 200) {
+            const authData = await authResponse.json();
+            const accessToken = authData.access_token;
 
-            const dayUptime = calculateTotalUptime(data, 24);  // 24 hours for a day
-            const monthUptime = calculateTotalUptime(data, 720);  // 720 hours for a month
-
-            return {
-                day: {
-                    raw: dayUptime,
-                    formatted: (dayUptime * 100).toFixed(2)
-                },
-                month: {
-                    raw: monthUptime,
-                    formatted: (monthUptime * 100).toFixed(4)
-                }
+            // Use access token in subsequent requests
+            const apiHeaders = {
+                'accept': 'application/json',
+                'Authorization': `Bearer ${accessToken}`,
             };
+
+            const apiResponse = await fetch(apiUrl, { method: 'GET', headers: apiHeaders });
+
+            if (apiResponse.status === 200) {
+                const data = await apiResponse.json();
+
+                const dayUptime = calculateTotalUptime(data, 24);
+                const monthUptime = calculateTotalUptime(data, 720);
+
+                return {
+                    day: {
+                        raw: dayUptime,
+                        formatted: (dayUptime * 100).toFixed(2),
+                    },
+                    month: {
+                        raw: monthUptime,
+                        formatted: (monthUptime * 100).toFixed(4),
+                    },
+                };
+            } else {
+                console.error('Error:', `Received unexpected status code: ${apiResponse.status}`);
+            }
         } else {
-            console.error('Error:', `Received unexpected status code: ${response.status}`);
+            console.error('Error:', `Received unexpected status code: ${authResponse.status}`);
         }
     } catch (error) {
         console.error('Error:', error.message);
@@ -42,6 +71,7 @@ function calculateTotalUptime(data, durationThreshold) {
     const totalUptime = filteredData.reduce((sum, record) => sum + record.uptime, 0);
     return totalUptime;
 }
+
 
 // Call the function to execute it
 module.exports();
