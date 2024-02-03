db006ccd655cbb221d0d228aa183412fe7a34ab2
David Moll <david.leander.moll@gmail.com>
2024-02-03 14:29:15 +0100

    Excludes net nuggets from collection

--- src/_collections/lastThreeArticles.js
+++ src/_collections/lastThreeArticles.js
@@ -1,7 +1,12 @@
 module.exports = function (collectionApi) {
-    const allArticles = collectionApi.getAll().filter(item => item.data.tags && item.data.tags.includes('posts')).sort((a, b) => {
+    const allArticles = collectionApi.getAll().filter(item => {
+        // Exclude posts with the tag "net nuggets"
+        const hasNetNuggetsTag = item.data.tags && item.data.tags.includes('net nuggets');
+        return item.data.tags && item.data.tags.includes('posts') && !hasNetNuggetsTag;
+    }).sort((a, b) => {
         return new Date(b.date) - new Date(a.date);
     });
+
     const lastThreeArticles = allArticles.slice(0, 3);
 
     return lastThreeArticles;
